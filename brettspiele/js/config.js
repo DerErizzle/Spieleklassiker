c029757f3f5ff7762df412191f913240ede4452b
Eric <eric.boingo@gmail.com>
2025-03-24 18:49:24 +0100

    AWS

--- brettspiele/js/config.js
+++ brettspiele/js/config.js
@@ -1,8 +1,12 @@
 const config = {
-    cdnBaseUrl: 'https://cdn-worker.eric-boingo.workers.dev',
-    assetVersion: '1.0.0'
+    cdnBaseUrl: 'https://erizzle-games-assets.s3.us-east-2.amazonaws.com',
+    assetVersion: '1.0.0',
 };
 
 function getCdnUrl(assetPath) {
-    return `${config.cdnBaseUrl}${assetPath}`;
+    return `${config.cdnBaseUrl}${assetPath}?v=${config.assetVersion}`;
+}
+
+function getFallbackUrl(assetPath) {
+    return `${config.fallbackBaseUrl}${assetPath.split('/').pop()}`;
 }
